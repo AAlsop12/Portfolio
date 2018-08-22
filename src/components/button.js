a61b1c5659d0520bf7c14c295d6487aeb198e3e1
AAslsop12 <aimee.johansen33@gmail.com>
2018-08-22 12:29:52 -0600

    added fly in animation to welcome component

--- src/components/button.js
+++ src/components/button.js
@@ -1,53 +1,25 @@
 import React, { Component } from 'react';
 
-import { connect } from 'react-redux';
-import * as actions from '../actions';
 import history from '../history';
 
 
 class Button extends Component {
     render() {
-  
+
+        const { type, input, onClick } = this.props;
+
         return (
-            <div className='button'>
-                {
-                    this.props.navbarLinks.forEach((link, index) => {
-                        return (
-                            <button className={'button__contact'} key={index} onClick={() => this.props.changeNavbarActive(link._id)}>
-                                CONTACT
-                            </button>
-                        )
-                    })
-                }
-            </div>
+         
+                <button className='button__contact'
+                        type={type}
+                        {...input}
+                        onClick={() => history.push('/contact')}>CONTACT</button>
+       
         )
 
     }
 }
 
-function mapStateToProps(state) {
-    const{ navbarLinks } = state.headerNavbar;
-    return {
-        navbarLinks
-    }
-}
-
-Button = connect(mapStateToProps, actions)(Button);
 
 export default Button;
 
-
-
-
-// const { type, input, onClick } = this.props;
-
-//         return (
-         
-//                 <button className='button__contact'
-//                         type={type}
-//                         {...input}
-//                         // onClick={() => history.push('/contact')}
-//                         key={index} 
-//                         onClick={() => this.props.changeNavbarActive(link._id)}>CONTACT</button>
-       
-//         )
--- src/components/layout.js
+++ src/components/layout.js
@@ -14,6 +14,7 @@ class Layout extends Component {
         <Header />
         <Navbar />
         <Account />
+        <Button />
         {this.props.children}
 
       </div>

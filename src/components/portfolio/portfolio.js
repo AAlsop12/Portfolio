cbdda60fc3f7a07b0ff8dce9af1240b6150584d0
AAslsop12 <aimee.johansen33@gmail.com>
2018-09-14 09:15:43 -0600

    added pictures to the portfolio

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -10,13 +10,17 @@ class Portfolio extends Component {
     render() {
         return (
             <div className='portfolio'>
-                {/* <PageTitle className='portfolio__title' title='Portfolio'/> 
+                {/* {/* <PageTitle className='portfolio__title' title='Portfolio'/>  */}
                 <Project className='ecommerce' title='Ecommmerce'/>
-                <Project className='madlibs' title='Madlibs'/> */}
+                <Project className='madlibs' title='Madlibs'/> 
                 <Project className='countdown' title='Countdown' link="https://protected-journey-60036.herokuapp.com/"/>
+                    <img className="countdown__pic" src="countdown.png" width='200' height='150'></img>
                 <Project className='schedule' title='Course Scheduling' link="https://course-scheduling.herokuapp.com"/>
+                    <img className="schedule__pic" src="schedule.png" width='200' height='150'></img>
                 <Project className='github' title='Github' link="https://github.com/AAlsop12"/>
+                    <img className="github__pic" src="github.png" width='200' height='150'></img>
                 <Project className='portfolio' title='Portfolio' link="https://desolate-sierra-42605.herokuapp.com/"/>
+                    <img className="portfolio__pic" src="portfolio.png" width='200' height='150'></img>
 
             </div>
         )
@@ -24,3 +28,5 @@ class Portfolio extends Component {
 }
 
 export default Portfolio;
+
+
